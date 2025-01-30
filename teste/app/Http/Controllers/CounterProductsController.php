7f454101903ed95201de7c8a2916b60be2a0796f
Moises Fausto <moisesfaustoo@gmail.com>
2025-01-30 01:48:28 -0300

    fix: Manter apenas o retorno do metodo

--- teste/app/Http/Controllers/CounterProductsController.php
+++ teste/app/Http/Controllers/CounterProductsController.php
@@ -3,33 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCounterProductsRequest;
-use App\Imports\CounterProductsImport;
-use App\Models\CounterProducts;
-use Illuminate\Http\JsonResponse;
-use Maatwebsite\Excel\Facades\Excel;
-use Throwable;
+use App\Services\CounterProductsService;
 
 class CounterProductsController extends Controller
 {
-    public function upload(StoreCounterProductsRequest $request): JsonResponse
+    public function __construct(protected CounterProductsService $counterProductsService)
     {
-        $counterProducts = CounterProducts::where('NameFile', $request->file('file')->getClientOriginalName())->first();
+    }
 
-        if ($counterProducts) {
-            return response()->json(['message' => 'CSV already imported'], JsonResponse::HTTP_FOUND);
-        }
-
-        try {
-            Excel::import(new CounterProductsImport(
-                $request->file('file')->getClientOriginalName()
-            ), $request->file('file'));
-
-            return response()->json(['message' => 'CSV imported successfully']);
-        } catch (Throwable $exceptions) {
-            return response()->json([
-                'code' => $exceptions->getCode(),
-                'error' => $exceptions->getMessage()
-            ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
-        }
+    public function upload(StoreCounterProductsRequest $request)
+    {
+        return $this->counterProductsService->uploadFile($request);
     }
 }
