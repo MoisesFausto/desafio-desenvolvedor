caa3e6cdbb053eb75e20cecead5a49c8ae6be19d
Moises Fausto <moisesfaustoo@gmail.com>
2025-02-01 03:43:53 -0300

    feat: criar camada de repository

---
+++ teste/app/Respositories/CounterProductsRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Respositories;
+
+use App\Contracts;
+use App\Models\CounterProducts;
+use Illuminate\Database\Eloquent\Collection;
+
+class CounterProductsRepository implements Contracts\CounterProductsRepository
+{
+    public function findByFileNameOrRptDt(?string $fileName, ?string $rptDt): Collection
+    {
+        return CounterProducts::select(['id', 'NameFile', 'RptDt', 'created_at as UploadAt'])
+            ->where('NameFile', '=', $fileName)
+            ->orWhere('RptDt', '=', $rptDt)
+            ->groupBy('NameFile')
+            ->get();
+    }
+
+    public function findByFileName(string $fileName): ?CounterProducts
+    {
+        return CounterProducts::where('NameFile', $fileName)->first();
+    }
+
+    public function findByParams(array $params): ?CounterProducts
+    {
+        $query = CounterProducts::select(['RptDt', 'TckrSymb', 'MktNm', 'SctyCtgyNm', 'ISIN', 'CrpnNm']);
+
+        foreach ($params as $key => $value) {
+            $query = $query->where($key, $value);
+        }
+
+        return $query->first();
+    }
+
+    public function getAllCounterProductsPaginate(int $paginate = 100)
+    {
+        return CounterProducts::select(
+            ['RptDt', 'TckrSymb', 'MktNm', 'SctyCtgyNm', 'ISIN', 'CrpnNm']
+        )->paginate($paginate);
+    }
+}
